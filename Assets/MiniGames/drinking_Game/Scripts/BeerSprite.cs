a094bb2c69d4017e33354676e78800c36132e190
Jacques Coetzee <jcoetzee2201@gmail.com>
2024-07-12 09:59:03 +0200

    minor changes

--- Assets/MiniGames/drinking_Game/Scripts/BeerSprite.cs
+++ Assets/MiniGames/drinking_Game/Scripts/BeerSprite.cs
@@ -11,7 +11,7 @@ public class BeerSprite : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        int num = UnityEngine.Random.Range(0, sprites.Length + 1);
+        int num = UnityEngine.Random.Range(0, sprites.Length);
         spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
         spriteRenderer.sprite = sprites[num];
     }
