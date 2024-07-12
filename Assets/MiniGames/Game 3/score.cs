c7220e0d17170f5e94995bc4f2f0fb8afd02b7c6
Daynavos <123731498+Daynavos@users.noreply.github.com>
2024-07-12 17:14:41 +0200

    end condition

--- Assets/MiniGames/Game 3/score.cs
+++ Assets/MiniGames/Game 3/score.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,16 +6,36 @@ using UnityEngine;
 public class score : MonoBehaviour
 {
     [SerializeField] private int OneScore = 0, TwoScore = 0;
-
+    private Input_Manager.PlayerNumber winner;
+    public int teeth = 7;
+    
     public void AddOneToOne()
     {
         OneScore++;
+        teeth--;
         Debug.Log(OneScore);
     }
     
     public void AddOneToTwo()
     {
         TwoScore++;
+        teeth--;
         Debug.Log(TwoScore);
+    }
+
+    private void Update()
+    {
+        if (teeth == 0)
+        {
+            if (OneScore > TwoScore)
+            {
+                winner = Input_Manager.PlayerNumber.P1;
+            }
+            else
+            {
+                winner = Input_Manager.PlayerNumber.P2;
+            }
+            SceneBehaviour.Instance.EndGameSession(winner);
+        }
     }
 }
