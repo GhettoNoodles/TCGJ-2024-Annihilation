9a9311aaf232d77f91ddf0bfc0505416bb1853ef
Jacques Coetzee <jcoetzee2201@gmail.com>
2024-07-13 11:25:09 +0200

    shooty ui

--- Assets/MiniGames/Shooty/shootyScore.cs
+++ Assets/MiniGames/Shooty/shootyScore.cs
@@ -4,17 +4,48 @@ using UnityEngine;
 
 public class shootyScore : MonoBehaviour
 {
+    [SerializeField] private GameObject holdPanel;
     public int playerOne = 0, playerTwo = 0;
     // Start is called before the first frame update
     void Start()
     {
+        SceneBehaviour.Instance.GameLoaded();
         playerOne = 0;
         playerTwo = 0;
     }
     // Update is called once per frame
     void Update()
     {
-        
+        if (Time.timeSinceLevelLoad >= SceneBehaviour.Instance.GameTime)
+        {
+            Input_Manager.PlayerNumber winner;
+            if (playerOne > playerTwo)
+            {
+                winner = Input_Manager.PlayerNumber.P1;
+            }
+            else if (playerOne < playerTwo)
+            {
+                winner = Input_Manager.PlayerNumber.P2;
+            }
+            else
+            {
+                winner = (Input_Manager.PlayerNumber)Random.Range(0, 2);
+            }
+
+            SceneBehaviour.Instance.EndGameSession(winner);
+        }
+
+        if (!(Input_Manager.Instance.Get_Hold(Input_Manager.PlayerNumber.P1) &&
+              Input_Manager.Instance.Get_Hold(Input_Manager.PlayerNumber.P2)))
+        {
+            holdPanel.SetActive(true);
+            Time.timeScale = 0f;
+        }
+        else
+        {
+            holdPanel.SetActive(false);
+            Time.timeScale = 1f;
+        }
     }
     
     public void incOne()
